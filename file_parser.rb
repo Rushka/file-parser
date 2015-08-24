138fd8a7fe980d802c844d1a98c18d3eeb0845b5
dub <vennde@yandex.ru>
2015-08-24 14:34:55 +0300

    base logic

--- file_parser.rb
+++ file_parser.rb
@@ -1,15 +1,33 @@
 class FileParser
-  ROOT = File.expand_path(File.dirname(__FILE__))
+  REG_ID = /U[0-9]+/
 
   def initialize(list)
     @list = Dir.glob(list)
+    @result = {}
   end
 
   def start
     @list.each do |file_path|
-      File.open file_path, 'r' do |file|
+      File.open(file_path, 'r') do |file|
         p file.read
       end
     end
+  end
+
+  def first_file
+    File.open(@list.first, 'r') do |file|
+      accounts = file.read.split(/\n/)
+      accounts.map do |line|
+        if line.match(REG_ID)
+          find_or_create_id line          
+        end
+      end
+    end
+  end
+
+  private
+  def find_or_create_id(line)
+    match = line.match(REG_ID)[0]
+    @result[match] ||= {} 
   end
 end
