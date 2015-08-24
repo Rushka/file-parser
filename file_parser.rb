e61c5d8833ec4c6e0cbb65b2ab5bd718b5ffb844
dub <vennde@yandex.ru>
2015-08-24 16:43:43 +0300

    types added

--- file_parser.rb
+++ file_parser.rb
@@ -2,9 +2,11 @@ require 'date'
 
 class FileParser
   TYPES = %w[account activity position security]
+  YEAR_RANGE = 1900..(Date.today.year + 50)
 
   REG_ID   = /U[0-9]+/
-  REG_DATE = /[0-9]{8}/
+  REG_DATE = /,([0-9]{8}),/
+  REG_TYPES = Regexp.new TYPES.map(&:capitalize).join('|')
 
   def initialize(list)
     @list = Dir.glob(list)
@@ -14,22 +16,22 @@ class FileParser
   def start
     @list.each do |file_path|
       File.open(file_path, 'r') do |file|
-        p file.read
+         file.read.split(/\n/).each do |line|
+          @line = line
+          start_chain if valid_line? line
+        end
       end
     end
+
+    p @result
   end
 
   def first_file
     File.open(@list.first, 'r') do |file|
-      accounts = file.read.split(/\n/)
-      accounts.map do |line|
-        if valid_line? line
-          @line = line
 
-          find_or_create_id 
-          find_or_create_date
-          # find_or_create_type
-        end
+      file.read.split(/\n/).each do |line|
+        @line = line
+        start_chain if valid_line? line
       end
 
       p @result
@@ -37,16 +39,33 @@ class FileParser
   end
 
   private
+  def start_chain
+    find_or_create_id
+  end
+
   def find_or_create_id
     match = @line.match(REG_ID)[0]
     @current_id = @result[match] ||= {} 
+
+    find_or_create_date
   end
 
   def find_or_create_date
     @line.scan(REG_DATE) do |date_string|
       date = str_to_date(date_string)
       next unless date
+
       @current_date = @current_id[date] ||= []
+
+      find_or_create_type
+    end
+  end
+
+  def find_or_create_type
+    @line.scan(REG_TYPES) do |type_string|
+      unless @current_date.include? type_string
+        @current_date << type_string
+      end
     end
   end
 
@@ -55,6 +74,15 @@ class FileParser
   end
 
   def str_to_date(string)
-    Date.strptime(string, "%Y%m%d").to_s rescue nil
+    begin
+      string = string.first.gsub(/,/, '')
+      date = Date.strptime(string, "%Y%m%d")
+   
+      raise unless YEAR_RANGE.include? date.year
+      
+      date.to_s
+    rescue 
+      nil
+    end
   end
 end
--- index.rb
+++ index.rb
@@ -2,4 +2,4 @@ require_relative "file_parser"
 
 parser = FileParser.new('parse_this/*')
 
-parser.first_file
+parser.start
